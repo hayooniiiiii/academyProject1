99895b94a83e618d8221129926db45d4728a4893
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-05-31 10:38:49 +0900

    우형쿤한테 빨대 꽂기 프로젝트

---
+++ src/main/java/com/example/demo/AcademyProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyProject1Application.class, args);
+	}
+
+}
