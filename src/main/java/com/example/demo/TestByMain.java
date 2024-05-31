796dfd71b8c3329443669eebe3c276aa8af48ade
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-05-31 12:42:59 +0900

    메인꺼합침

--- src/main/java/com/example/demo/TestByMain.java
+++ src/main/java/com/example/demo/TestByMain.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class TestByMain {
 	public void wirteMain() {
 		System.out.println("메인 브렌치로 작업했습니다.");
+		System.out.println("son이 수정-메인");
 	}
 
 }
--- src/main/java/com/example/demo/TestBySon.java
+++ src/main/java/com/example/demo/TestBySon.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class TestBySon {
 	public void writeSon() {
 		System.out.println("son:게시판 만드는중");
+		System.out.println("son이 한번더수정");
 	}
 }
